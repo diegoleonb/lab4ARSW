8438d7ce539093d5c46ab7579c0b80bdedc9e324
diegoleonb <diegoleonb10@gmail.com>
2022-08-31 09:32:16 -0500

    Se modifica SpanishSpellChecker

--- lab4ARSW1.1/src/main/java/edu/eci/arsw/springdemo/SpanishSpellChecker.java
+++ lab4ARSW1.1/src/main/java/edu/eci/arsw/springdemo/SpanishSpellChecker.java
@@ -2,6 +2,7 @@ package edu.eci.arsw.springdemo;
 
 import java.util.LinkedHashMap;
 import java.util.Map;
+import org.springframework.stereotype.Service;
 
 @Service
 public class SpanishSpellChecker implements SpellChecker {
